e0e0eadabbf452eeac14659f80867776ab1a9034
Anton-I-Chumakov <anton.chumakov92@gmail.com>
2026-05-23 20:22:19 +0300

    Update Readme.md

--- Pioneer/include/pioneer/Support.hpp
+++ Pioneer/include/pioneer/Support.hpp
@@ -1,3 +1,22 @@
+/* *****************************************************************************
+PioneerEngine, a test game engine
+Copyright (C) 2026  Chumakov Anton
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+***************************************************************************** */
+
 #ifndef PIONEER_SUPPORT_HPP
 #define PIONEER_SUPPORT_HPP
 
@@ -14,19 +33,5 @@
 #       define PIONEER_API
 #   endif
 #endif
-
-//#ifdef PIONEER_LIBRARY_BUILD
-//#   ifdef _WIN32
-//#       define PIONEER_API __declspec(dllexport)
-//#   else
-//#       define PIONEER_API __attribute__((visibility("default")))
-//#   endif
-//#else
-//#   ifdef _WIN32
-//#       define PIONEER_API __declspec(dllimport)
-//#   else
-//#       define PIONEER_API
-//#   endif
-//#endif
 
 #endif // !PIONEER_SUPPORT_HPP
